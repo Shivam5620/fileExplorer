55de148f98f6202d76809ae4f062227ea00cd58e
sivam5620 <sbirla04082000@gmail.com>
2024-07-31 23:18:18 +0530

    >..

--- codes/src/redux/actions.js
+++ codes/src/redux/actions.js
@@ -1,6 +1,13 @@
 export const ADD_ITEM = 'ADD_ITEM';
 export const RENAME_ITEM = 'RENAME_ITEM';
 export const DELETE_ITEM = 'DELETE_ITEM';
+// actions.js
+export const SELECT_FILE = 'SELECT_FILE';
+
+export const selectFile = (file) => ({
+  type: SELECT_FILE,
+  payload: file,
+});
 
 export const addItem = (parentPath, newItem) => ({
   type: ADD_ITEM,
--- codes/src/redux/reducer.js
+++ codes/src/redux/reducer.js
@@ -1,15 +1,71 @@
-import FileExploere from "../components/FileExploere";
-import data from '../data/data.json'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
-const App = () => {
-  
-  return (
-    <div className="">
+// reducer.js
+import { ADD_ITEM, DELETE_ITEM, RENAME_ITEM } from "./actions";
 
-    <FileExploere FolderData={data}/>
-     <ToastContainer />
-    </div>
-  )
+const initialState = {
+  files: [
+    // Initial folder structure
+    {
+      id: "1",
+      name: "src",
+      type: "folder",
+      children: [
+        { id: "2", name: "App.js", type: "file" },
+        { id: "3", name: "index.js", type: "file" },
+      ],
+    },
+  ],
 };
-export default App;
+
+const findItemById = (items, itemId) => {
+  for (const item of items) {
+    if (item.id === itemId) return item;
+    if (item.children) {
+      const found = findItemById(item.children, itemId);
+      if (found) return found;
+    }
+  }
+  return null;
+};
+
+const fileReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ITEM: {
+      const { parentId, newItem } = action.payload;
+      const updatedFiles = [...state.files];
+      const parent = findItemById(updatedFiles, parentId);
+      if (parent && parent.type === "folder") {
+        parent.children = [...(parent.children || []), newItem];
+      }
+      return { ...state, files: updatedFiles };
+    }
+
+    case DELETE_ITEM: {
+      const updatedFiles = [...state.files];
+      const deleteRecursive = (items, itemId) => {
+        return items.filter((item) => {
+          if (item.id === itemId) return false;
+          if (item.children) {
+            item.children = deleteRecursive(item.children, itemId);
+          }
+          return true;
+        });
+      };
+      return { ...state, files: deleteRecursive(updatedFiles, action.payload) };
+    }
+
+    case RENAME_ITEM: {
+      const { itemId, newName } = action.payload;
+      const updatedFiles = [...state.files];
+      const item = findItemById(updatedFiles, itemId);
+      if (item) {
+        item.name = newName;
+      }
+      return { ...state, files: updatedFiles };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default fileReducer;
